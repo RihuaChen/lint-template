38769df09b0b075742374f8da29d4d7329c781fd
ricky chen <rickychenrihua@foxmail.com>
2021-01-13 17:09:37 +0800

    update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: [require.resolve('@umijs/fabric/dist/eslint')],
   // in antd-design-pro
+  ignorePatterns: ['.eslintrc.js', 'i18n'],
   globals: {
     page: true,
   },
